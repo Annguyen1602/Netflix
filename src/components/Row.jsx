faf8c248182c11955b3545e180128a9e37bcdc00
annguyen <nguyentruongan16021996@gmail.com>
2022-12-08 00:00:26 +0700

    update

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,33 +1,29 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import {MdChevronLeft, MdChevronRight} from 'react-icons/md'
+import Movie from "./Movie";
 
 const Row = ({ title, fetchURL }) => {
-    const [movies, setMovies] = useState([]);
-
-    useEffect(()=>{
-        axios.get(fetchURL).then((response)=>{
-            setMovies(response.data.results)
-        })
-
-    },[fetchURL])
-    console.log(movies);
-
-
+  const [movies, setMovies] = useState([]);
+  
+  useEffect(() => {
+    axios.get(fetchURL).then((response) => {
+      setMovies(response.data.results);
+    });
+  }, [fetchURL]);
+  console.log(movies);
 
   return (
     <>
       <h1 className="text-white font-bold md:text-xl p-4">{title}</h1>
       <div className="relative flex items-center ">
-        <div id={'slider'}>
-            {movies.map((item,id)=>(
-                <div className="w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor-pointer relative p-2" key={id}>
-                    <img src={`https://image.tmdb.org/t/p/w500/${item?.backdrop_path}`} alt={item?.title} />
-
-                </div>
-
-            ))}
+        <MdChevronRight size={40} className="bg-white rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer "/>
+        <div id={"slider"} className="w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide relative">
+          {movies.map((item, id) => (
+            <Movie item={item} key={id}/>
+          ))}
         </div>
-
+        <MdChevronLeft size={40} className="bg-white rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer"/>
       </div>
     </>
   );
